5f4379d06eaf06461345754da2246704ce4efbd6
Lars van Asseldonk <lars.vanasseldonk@nslogin.nl>
2022-11-21 07:44:53 +0100

    restructure folder

--- triggers/__init__.py
+++ triggers/__init__.py
@@ -3,12 +3,12 @@ import datetime
 
 import azure.functions as func
 
-from birthday_notifier.src.main import run_daily_trigger
+from src.daily_check import run_daily_trigger
 
 def main(dayTrigger: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
-    run_daily_trigger.main()
+    run_daily_trigger()
 
     if dayTrigger.past_due:
         logging.info('The timer is past due!')
